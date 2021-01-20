6982e212679884018bddfe6a5fb6c359004c0010
alexpaul <alexpaulnyc@gmail.com>
2021-01-20 16:48:15 -0500

    updates

--- Play-YouTube-Video/YouTubeVidoeViewController.swift
+++ Play-YouTube-Video/YouTubeVidoeViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class YouTubeVidoeViewController: UIViewController {
   
+  let movieIds = ["X1rJvSF3l6k", "LbKs766Tr88", "eZ4xDTz8Avc", "5wfrDhgUMGI", "ZprXMxKg--w"]
+  
   @IBOutlet weak var youTubePlayerView: YTPlayerView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    youTubePlayerView.load(withVideoId: "X1rJvSF3l6k")
+    youTubePlayerView.load(withVideoId: movieIds.randomElement() ?? "X1rJvSF3l6k")
   }
 
 }
